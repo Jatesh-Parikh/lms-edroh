0668328fa1c9d7d39a52c5c90f62a32f3928fafa
Jatesh Parikh <jateshparikh97@gmail.com>
2026-01-15 22:18:01 +0530

    add create stripe payment intent controller

---
+++ server/src/controllers/transactionController.ts
@@ -0,0 +1,49 @@
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+import Transaction from "../models/transactionModel";
+import UserCourseProgress from "../models/userCourseProgressModel";
+
+dotenv.config();
+
+if(!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY as required but was not found in env variables");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export const listTransactions = async (req: Request, res: Response): Promise<void> => {
+    const { userId } = req.query;
+
+    try {
+        const transactions = userId ? await Transaction.query("userId").eq(userId).exec() : await Transaction.scan().exec();
+        
+        res.json({ message: "Transactions retrieved successfully", data: transactions });
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving transactions", error });
+    }
+}
+
+export const createStripePaymentIntent = async(req: Request, res: Response): Promise<void> => {
+    let { amount } = req.body;
+
+    if(!amount || amount <= 0) {
+        amount = 50;
+    }
+
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "usd",
+            automatic_payment_methods: {
+                enabled: true,
+                allow_redirects: "never"
+            }
+        });
+
+        res.json({ message: "", data: { clientSecret: paymentIntent.client_secret } });
+    } catch (error) {
+        res.status(500).json({ message: "Error creating stripe payment intent", error });
+    }
+}
