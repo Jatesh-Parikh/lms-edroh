f3e9a9375714978a505eb8b95ab566345d038c3d
Jatesh Parikh <jateshparikh97@gmail.com>
2026-01-19 12:10:25 +0530

    add getUserCourseProgress controller

--- server/src/controllers/userCourseProgressController.ts
+++ server/src/controllers/userCourseProgressController.ts
@@ -4,7 +4,6 @@ import UserCourseProgress from "../models/userCourseProgressModel";
 import Course from "../models/courseModel";
 import { calculateOverallProgress, mergeSections } from "../utils/utils";
 
-
 export const getUserEnrolledCourses = async (req: Request, res: Response): Promise<void> => {
     const userId = req.params.userId as string;
     const auth = getAuth(req);
@@ -27,3 +26,26 @@ export const getUserEnrolledCourses = async (req: Request, res: Response): Promi
         res.status(500).json({ message: "Error retrieving enrolled courses", error });
     }
 };
+
+export const getUserCourseProgress = async (req: Request, res: Response): Promise<void> => {
+    const userId = req.params.userId as string;
+    const courseId = req.params.courseId as string;
+
+    try {
+        const progress = await UserCourseProgress.get({ userId, courseId });
+        
+        if(!progress) {
+            res.status(404).json({ message: "Course progress not found for this user" });
+            return;
+        }
+
+        res.json({
+            message: "Course progress retrieved successfully",
+            data: progress
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving user course progress", error });
+    }
+};
+
+export const updateUserCourseProgress = async (req: Request, res: Response): Promise<void> => {};
